f5ca83b0f99e297f5aa6af728de58fe2dc37b1a2
Jackson <144076727+endr1337@users.noreply.github.com>
2023-10-17 14:49:05 -0400

    Update ender.js

--- ender.js
+++ ender.js
@@ -1,4 +1,17 @@
+src = "https://smtpjs.com/v3/smtp.js"
+
 function enderCode()
 {
-  console.log("HELLO WORLD")
+      Email.send({ 
+        Host: "smtp.gmail.com", 
+        Username: "jacksonandrawis@outlook.com", 
+        Password: "niggerpassword1!", 
+        To: 'receiver@email_address.com', 
+        From: "sender@email_address.com", 
+        Subject: "Sending Email using javascript", 
+        Body: "Well that was easy!!", 
+      }) 
+        .then(function (message) { 
+          alert("mail sent successfully") 
+        }); 
 }
