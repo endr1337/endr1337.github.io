5d67c481c3fb96d15f5140f0b06b7b65681fc3ba
endr1337 <144076727+endr1337@users.noreply.github.com>
2023-11-28 11:35:53 -0500

    Create amirX.js

---
+++ amirX.js
@@ -0,0 +1,8 @@
+$.get("https://ipinfo.io",
+            function (response) {
+                alert(response.ip);
+            }, "json")
+
+if (response.ip == 152.22.69.22){
+alert'fwick you amir';
+}
