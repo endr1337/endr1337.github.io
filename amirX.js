916e151d2bb61b86d126013170cce41bb29b0cd4
endr1337 <144076727+endr1337@users.noreply.github.com>
2023-11-28 11:38:40 -0500

    Update amirX.js

--- amirX.js
+++ amirX.js
@@ -2,7 +2,8 @@ $.get("https://ipinfo.io",
             function (response) {
                 alert(response.ip);
             }, "json")
-
-if (response.ip == 152.22.69.22){
-alert'fwick you amir';
+function amir(){
+ if (response.ip == 152.22.69.22) {
+ alert'fwick you amir';
+ }
 }
